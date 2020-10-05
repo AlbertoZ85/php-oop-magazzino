33fa24bc4ce83a417335c70535d9f35de48564fa
Alberto Zordan <zorberto@gmail.com>
2020-10-05 16:28:16 +0200

    Date bug fixed

--- classes/Perishable.php
+++ classes/Perishable.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'Goods.php';
+// require_once 'Goods.php'; # non serve perché non modifico Goods
 
 class Perishable extends Goods {
     public function __construct($_barcode, $_brand, $_name, $_expiryDate) {
@@ -10,10 +10,14 @@ class Perishable extends Goods {
     public function totalsAndDate($_quantity, $expiryDate) {
         $totals = parent::totals($_quantity);
 
-        $currentDate = new DateTime(date('Y-m-d'));
-        $expiryDate = new DateTime($expiryDate);
-        $daysToExp = $currentDate->diff($expiryDate);
-        $daysToExp->days;
+        // $currentDate = new DateTime(date('Y-m-d'));
+        // $expiryDate = new DateTime($expiryDate);
+        // $daysToExp = $currentDate->diff($expiryDate);
+        // $daysToExp->days;
+
+        $currentDate = date('Y-m-d');
+        $diff = abs(strtotime($expiryDate) - strtotime($currentDate));
+        $daysToExp = $diff / (60 * 60 * 24);
 
         return [$totals, $daysToExp];
     }
